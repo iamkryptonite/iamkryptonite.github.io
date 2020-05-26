690065c2bc6743e1797c015f1c2270bd6bc84090
iamkryptonite <iamkryptonite@users.noreply.github.com>
2020-05-26 10:59:42 +0530

    add responsive navbar

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {
+        edge: 'right',
+        draggable: false,
+        inDuration: 250,
+        outDuration: 200,
+        onOpenStart: null,
+        onOpenEnd: null,
+        onCloseStart: null,
+        onCloseEnd: null,
+        preventScrolling: false
+    });
+  });
